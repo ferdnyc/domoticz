97df10db2b3ffc9d8c7b2b4a9af1caabacd8a1e1
kiddigital <kiddigital@gmail.com>
2023-02-05 19:57:03 +0100

    Add TOTP commandline flag (to be removed)

--- main/domoticz.cpp
+++ main/domoticz.cpp
@@ -735,6 +735,19 @@ int main(int argc, char**argv)
 				FixFolderEnding(szStartupFolder);
 			}
 		}
+		if (cmdLine.HasSwitch("-totp"))
+		{
+			if (cmdLine.GetArgumentCount("-totp") != 1)
+			{
+				_log.Log(LOG_ERROR, "Please specify a TOTP secret (base32 encoded)");
+				return 1;
+			}
+			std::string szroot = cmdLine.GetSafeArgument("-totp", 0, "");
+			if (!szroot.empty())
+			{
+				iamserver_settings.totpsecret = szroot;
+			}
+		}
 	}
 
 	if (!logfile.empty())
